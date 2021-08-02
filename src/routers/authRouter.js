dc91e11381ddce2b24ed77d06acdab128985a574
Calvin Remsburg <cremsburg@protonmail.com>
2021-08-02 16:54:17 +0000

    cleanup CSS and HTML

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -6,43 +6,43 @@ const passport = require('passport');
 const authRouter = express.Router();
 
 authRouter.route('/signUp').post((req, res) => {
-  const { username, password } = req.body;
-  const url =
-    'mongodb://localhost:27017';
-  const dbName = 'junosConfig';
+    const { username, password } = req.body;
+    const url =
+        'mongodb://localhost:27017';
+    const dbName = 'junosConfig';
 
-  (async function addUser() {
-    let client;
-    try {
-      client = await MongoClient.connect(url);
+    (async function addUser() {
+        let client;
+        try {
+            client = await MongoClient.connect(url);
 
-      const db = client.db(dbName);
-      const user = { username, password };
-      const results = await db.collection('users').insertOne(user);
-      debug(results);
-      req.login(results.ops[0], () => {
-        res.redirect('/sessions');
-      });
-    } catch (error) {
-      debug(error);
-    }
-    client.close();
-  })();
+            const db = client.db(dbName);
+            const user = { username, password };
+            const results = await db.collection('users').insertOne(user);
+            debug(results);
+            req.login(results.ops[0], () => {
+                res.redirect('/');
+            });
+        } catch (error) {
+            debug(error);
+        }
+        client.close();
+    })();
 });
 
 authRouter
-  .route('/signIn')
-  .get((req, res) => {
-    res.render('signin');
-  })
-  .post(
-    passport.authenticate('local', {
-      successRedirect: '/sessions',
-      failureRedirect: '/',
+    .route('/signIn')
+    .get((req, res) => {
+        res.render('signin');
     })
-  );
+    .post(
+        passport.authenticate('local', {
+            successRedirect: '/',
+            failureRedirect: '/',
+        })
+    );
 authRouter.route('/profile').get((req, res) => {
-  res.json(req.user);
+    res.json(req.user);
 });
 
 module.exports = authRouter;
